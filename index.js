803ba0fb352fbfc498c5e8b7c5e36de32c8b5955
Gabriel Cabanillas Cabanillas <gabrielcabanillascabanillas@gmail.com>
2026-03-24 20:21:56 -0700

    Se agrego endpoint GET /usuario

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
 
+// Ruta base
 app.get('/', (req, res) => {
-  res.send('Hola mundo desde Express');
+  res.send('API funcionando');
 });
 
+// ENDPOINT Creado
+app.get('/usuario', (req, res) => {
+  const usuario = {
+    id: 1,
+    nombre: 'Juan',
+    rol: 'Administrador'
+  };
+
+  res.json(usuario);
+});
+
+// Servidor
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
